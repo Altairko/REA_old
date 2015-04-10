4ef6ac74446160245ebc97b10d08a57aeb6ab81f
Dmitriy Maskaev <maskaevda@gmail.com>
2015-04-10 21:40:04 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,6 @@
 <?php
-<<<<<<< HEAD
- $db = mysql_connect ("localhost","root","DnS357203");
+$db = mysql_connect ("localhost","root","DnS357203");
 mysql_select_db ("rea",$db);
-=======
- $db=@mysqli_connect("localhost", "root", "pass", "rea");
-  //mysql_select_db ("rea",$db);
->>>>>>> origin/master
 if (!$db) { echo ("<center><div class='alert alert-danger'>
         <strong>Соединение с базой данных не установлено. Обратитесь к вашему сис.админу.</strong>
       </div></center>
